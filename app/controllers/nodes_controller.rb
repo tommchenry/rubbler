3a23bd63eba769dd3a503c06e1d484d28335651e
Tom McHenry <tommchenry@gmail.com>
2017-03-07 21:19:45 -0600

    Add new node form. (#26)

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,5 +1,23 @@
 class NodesController < ApplicationController
   def index
+    @node = Node.new
     @nodes = Node.all
+  end
+
+  def create
+    @node = Node.new(node_params)
+    if @node.save
+      flash[:notice] = "Node created successfully."
+    else
+      flash[:notice] = "ERROR: Node not created."
+    end
+    redirect_to '/nodes'
+  end
+
+
+  private
+
+  def node_params
+    params.require(:node).permit(:name, :text, :img_url, :parent_id)
   end
 end
